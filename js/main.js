af46de498ed6878c4e093f25f9beebd46f9f7a50
ConfLost <33156540+SteadChaPE@users.noreply.github.com>
2019-03-26 19:56:18 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "March 2, 2019 18:30:00",
+	date: "March 27, 2019 18:30:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
